0eac94726d150b76433c57a70c7e4126cabc48ca
Piotr Baran <PiotrBy>
2020-04-02 23:14:04 +0200

    zadanie 9_5

---
+++ zadanie9_5/Complex.cpp
@@ -0,0 +1,5 @@
+//
+// Created by piotr on 02.04.2020.
+//
+
+#include "Complex.h"
---
+++ zadanie9_5/Complex.h
@@ -0,0 +1,14 @@
+//
+// Created by piotr on 02.04.2020.
+//
+
+#ifndef ZADANIE9_5_COMPLEX_H
+#define ZADANIE9_5_COMPLEX_H
+
+
+class Complex {
+
+};
+
+
+#endif //ZADANIE9_5_COMPLEX_H
